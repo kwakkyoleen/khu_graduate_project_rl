6c688904ef0b531b286c07adfdbd89800dacfc85
kwakkyoleen <2007kkl@naver.com>
2024-11-25 22:26:46 +0900

    14번째

--- standalone/ppo/ppo_test.py
+++ standalone/ppo/ppo_test.py
@@ -144,7 +144,7 @@ def main():
     #####################################################
 
     ################### checkpointing ###################
-    run_num_pretrained = 13     #### change this to prevent overwriting weights in same env_name folder
+    run_num_pretrained = 14     #### change this to prevent overwriting weights in same env_name folder
 
     directory = "PPO_preTrained"
     if not os.path.exists(directory):
